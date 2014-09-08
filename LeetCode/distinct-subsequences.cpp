238e9719d8dec81b074cb5389599e958e66f9a5d
Runwei Qiang <qiangrw@gmail.com>
2014-09-08 23:47:45 +0800

    [dynamic programming] add distinct subsequences.

---
+++ LeetCode/distinct-subsequences.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numDistinct(string S, string T) {
+        int m = S.size();
+        int n = T.size();
+        vector<int> f(n+1, 0);
+        f[0] = 1;
+        for (int i = 0; i < m; ++i) {
+            for (int j = n-1; j >= 0; --j) {
+                f[j+1] += (S[i] == T[j]) ? f[j] : 0;
+            }
+        }
+        return f[n];
+    }
+};
