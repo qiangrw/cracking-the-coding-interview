1af571e0d76e75405e083be6ef45fb4cc561222c
Runwei Qiang <qiangrw@gmail.com>
2014-09-11 11:33:59 +0800

    improve balanced binary tree, O(n)

--- LeetCode/balanced-binary-tree.cpp
+++ LeetCode/balanced-binary-tree.cpp
@@ -1,12 +1,21 @@
 class Solution {
     public:
+        /*int depth(TreeNode *root) {
+          if (root == nullptr) return 0;
+          return max(depth(root->left), depth(root->right)) + 1;
+          }*/
+
         int depth(TreeNode *root) {
             if (root == nullptr) return 0;
-            return max(depth(root->left), depth(root->right)) + 1;
+            int left_len = depth(root->left);
+            int right_len = depth(root->right);
+            if (left_len < 0 || right_len < 0 || abs(left_len - right_len) > 1) return -1;
+            return max(left_len, right_len) + 1;
         }
 
         bool isBalanced(TreeNode *root) {
             if (root == nullptr) return true;
-            return (isBalanced(root->left) && isBalanced(root->right) && abs(depth(root->left) - depth(root->right)) <= 1);
+            //return (isBalanced(root->left) && isBalanced(root->right) && abs(depth(root->left) - depth(root->right)) <= 1);
+            return depth(root) != -1;
         }
 };
