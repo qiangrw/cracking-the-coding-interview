0d0d92b8578db41dc433fb3277997f2e1be2fab0
Runwei Qiang <qiangrw@gmail.com>
2014-09-18 10:55:27 +0800

    update unique paths, using O(n) memory

--- LeetCode/unique-paths.cpp
+++ LeetCode/unique-paths.cpp
@@ -1,5 +1,7 @@
 class Solution {
     public:
+        /* 
+        // f(i, j) = f(i-1, j) + f(i, j-1)
         int uniquePaths(int m, int n) {
             vector<vector<int> > f(m, vector<int>(n, 0));
             for (int i = 0; i < m; ++i) f[i][0] = 1;
@@ -10,5 +12,16 @@ class Solution {
                 }
             }
             return f[m-1][n-1];
+        }
+        */
+
+        int uniquePaths(int m, int n) {
+            vector<int> f(n, 1);
+            for (int i = 1; i < m; ++i) {
+                for (int j = 1; j < n; ++j) {
+                    f[j] = f[j] + f[j-1];
+                }
+            }
+            return f[n-1];
         }
 };
