03b42c04e3dd6e40c76619cf881d8eef89509a75
Runwei Qiang <qiangrw@gmail.com>
2014-09-20 15:15:51 +0800

    jump game, neat version

--- LeetCode/jump-game-ii.cpp
+++ LeetCode/jump-game-ii.cpp
@@ -1,6 +1,19 @@
 class Solution {
     public:
         int jump(int A[], int n) {
+            int cur = 0, last = 0;
+            int step = 0;
+            for (int i = 0; i < n; i++) {
+                if (i > last) {
+                    last = cur;
+                    ++step;
+                }
+                cur = max(cur, i + A[i]);
+            }
+            return step;
+        }
+
+        /*int jump(int A[], int n) {
             int curlen = 1, nextlen = 0;
             int step = 0;
             int i = 0;
@@ -17,5 +30,5 @@ class Solution {
                 ++step;
             }
             return -1;
-        }
+        }*/
 };
