5b06606abe2940f2afb5ffb59f28eca2951bac7f
Runwei Qiang <qiangrw@gmail.com>
2014-09-21 16:05:53 +0800

    4sum

---
+++ LeetCode/4sum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    public:
+        vector<vector<int> > fourSum(vector<int> &num, int target) {
+            vector<vector<int> > res;
+            if (num.size() <= 3) return res;
+            sort(num.begin(), num.end());
+            auto last = num.end();
+            for (auto a = num.begin(); a < prev(last, 3); ++a) {
+                for (auto b = a + 1; b < prev(last, 2); ++b) {
+                    auto c = next(b, 1);
+                    auto d = prev(last, 1);
+                    while (c < d) {
+                        if (*a + *b + *c + *d == target) {
+                            res.push_back({*a, *b, *c, *d});
+                            ++c;
+                            --d;
+                        } else if (*a + *b + *c + *d < target) {
+                            ++c;
+                        } else {
+                            --d;
+                        }
+                    }
+                }
+            }
+            sort(res.begin(), res.end());
+            res.erase(unique(res.begin(),res.end()), res.end());
+            return res;
+        }
+};
