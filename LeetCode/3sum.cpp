1e616a61331cdd06c36b8439a96149e537b268d6
Runwei Qiang <qiangrw@gmail.com>
2014-09-21 15:31:57 +0800

    3sum, squeeze theorem

---
+++ LeetCode/3sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    public:
+        vector<vector<int> > threeSum(vector<int> &num) {
+            vector<vector<int> > res;
+            if (num.size() <= 2) return res;
+
+            sort(num.begin(), num.end());
+            for (auto a = num.begin(); a != prev(num.end(), 2); ++a) {
+                auto b = next(a);
+                auto c = prev(num.end());
+                while(b < c) {
+                    if (*a + *b + *c == 0) {
+                        vector<int> temp;
+                        temp.push_back(*a);
+                        temp.push_back(*b);
+                        temp.push_back(*c);
+                        res.push_back(temp);
+                        b++;
+                        c--;
+                    } else if (*a + *b + *c < 0) {
+                        b++;
+                    } else {
+                        c--;
+                    }
+                }
+            }
+            // remvoe duplicate
+            sort(res.begin(), res.end());
+            res.erase(unique(res.begin(), res.end()), res.end());
+            return res;
+        }
+};
