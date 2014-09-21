a2c8ad3d5a5da57e35b1d1312573a9f372eb3621
Runwei Qiang <qiangrw@gmail.com>
2014-09-21 16:02:49 +0800

    3sum closest

---
+++ LeetCode/3sum-closest.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    public:
+        int threeSumClosest(vector<int> &num, int target) {
+            int res = INT_MAX;
+            if (num.size() < 3) return res;
+            sort(num.begin(), num.end());
+
+            int min_gap = INT_MAX;
+            for (auto a = num.begin(); a < prev(num.end(), 2); ++a) {
+                auto b = a + 1;
+                auto c = num.end() - 1;
+                while (b < c) {
+                    int sum = *a + *b + *c;
+                    int gap = abs(sum - target);
+                    if (gap < min_gap) {
+                        min_gap = gap;
+                        res = sum;
+                        if (min_gap == 0) return res;
+                    }
+                    if (sum < target) ++b;
+                    else --c;
+                }
+            }
+            return res;
+        }
+};
